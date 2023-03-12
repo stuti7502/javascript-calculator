86ab31ac0f3228fc82cac71f65cee8d42d81c27e
stutivithlani <125354111+stutivithlani@users.noreply.github.com>
2023-03-12 23:10:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,25 @@
-// function myFunction1() {
-//     document.getElementById("drop1").classList.toggle("show");
-//     }
 
-//     // Close the dropdown if the user clicks outside of it
-//     window.onclick = function(event) {
-//     if (!event.target.matches('.btn1')) {
-//         var dropdowns = document.getElementsByClassName("drop-content1");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//             openDropdown.classList.remove('show');
-//         }
-//         }
-//     }
-//     }
-// 	function myFunction2() {
-// 		document.getElementById("drop2").classList.toggle("show");
-// 		}
-	
-// 		// Close the dropdown if the user clicks outside of it
-// 		window.onclick = function(event) {
-// 		if (!event.target.matches('.btn2')) {
-// 			var dropdowns = document.getElementsByClassName("drop-content2");
-// 			var i;
-// 			for (i = 0; i < dropdowns.length; i++) {
-// 			var openDropdown = dropdowns[i];
-// 			if (openDropdown.classList.contains('show')) {
-// 				openDropdown.classList.remove('show');
-// 			}
-// 			}
-// 		}
-// 		}
-// var text = ""
-// function display(x){
-// 	text += x;
-// 	document.getElementById('display').value = text;
-// }
-// var val = "";
+let x=0;
 var display1 = document.getElementById('display');
 var buttons = document.querySelectorAll("button");
+var previous = document.getElementById('previous');
 
 buttons.forEach((item) => {
 	
 	item.addEventListener('click', (e) => {
 		console.log("id",item.id);
+		
 		if(e.target.value == '='){
-			document.getElementById('previous').value = display1.value;
+			previous.value = display1.value;
 			val = eval(display1.value);
 			display1.value = val;
 			
+		}
+		else if(e.target.value == 'deg'){
+			display1.value = "";
+		}
+		else if(e.target.value == 'rad'){
+			display1.value = "";
 		}
 		else if(e.target.value == 'C'){
 			val = " ";
@@ -65,22 +35,67 @@ buttons.forEach((item) => {
 	}
 )}
 ); 
+// function myFunction1() {
+//     var click = document.getElementById("drop1");
+//     if(click.style.display ==="none") {
+//        click.style.display ="block";
+//     } else {
+//        click.style.display ="none";
+//     } 
+//  }
 function myFunction1() {
-    var click = document.getElementById("drop1");
-    if(click.style.display ==="none") {
-       click.style.display ="block";
+    document.getElementById("drop1").classList.toggle("show");
+    }
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+    if (!event.target.matches('.btn1')) {
+        var dropdowns = document.getElementsByClassName("drop-content1");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+        }
+    }
+    }
+function myFunction2() {
+	document.getElementById("drop2").classList.toggle("show");
+	}
+	
+	// Close the dropdown if the user clicks outside of it
+	window.onclick = function(event) {
+	if (!event.target.matches('.btn2')) {
+		var dropdowns = document.getElementsByClassName("drop-content2");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+			openDropdown.classList.remove('show');
+		}
+		}
+	}
+	}
+ function degtorad(){
+	var click = document.getElementById("deg");
+	var click1 = document.getElementById("rad");
+    if(click1.style.display ==="none") {
+       click1.style.display ="block";
+	   click.style.display ="none";
     } else {
-       click.style.display ="none";
-    } 
- }
- function myFunction2() {
-    var click = document.getElementById("drop2");
-    if(click.style.display ==="none") {
        click.style.display ="block";
-    } else {
-       click.style.display ="none";
+	   click1.style.display ="none";
     } 
- }
+}
+//  function myFunction2() {
+//     var click = document.getElementById("drop2");
+//     if(click.style.display ==="none") {
+//        click.style.display ="block";
+//     } else {
+//        click.style.display ="none";
+//     } 
+//  }
 
 function change(){
 	const col1 = document.getElementsByClassName("option1");
@@ -156,16 +171,9 @@ function hyp(){
 	}
 };
 
-function sin(){
-	display1.value = Math.sin(display1.value);
-}
-function cos(){
-	display1.value = Math.cos(display1.value);
-}
-function tan(){
-	display1.value = Math.tan(display1.value);
-}
+
 function fact(){
+	previous.value = "fact("+display1.value+")";
 	if (display1.value < 0) {
 		display1.value = "Error!";
 	}
@@ -188,9 +196,11 @@ function plusminus(){
 		display1.value = -(display1.value)
 }
 function square(){
+	previous.value = "sqr("+display1.value+")";
 	display1.value = display1.value ** 2;
 }
 function cube(){
+	previous.value = "cube("+display1.value+")";
 	display1.value = display1.value ** 3;
 }
 function tenpowerx(){
@@ -217,17 +227,133 @@ function cbroot(){
 function onebyx(){
 	display1.value = 1/display1.value;
 }
-function positive(){
-	if(display1.value<0){
-		display1.value = -(display1.value);
-	}
-	else{
-		display1.value;
-	}
+function abs(){
+	display1.value = Math.abs(display1.value);
 }
 function floor(){
 	display1.value = Math.floor(display1.value);
 }
 function ceil(){
 	display1.value = Math.ceil(display1.value);
+}
+function sin(){
+	if(document.getElementById('deg').value="deg"){
+		previous.value = "sin("+display1.value+")";
+		display1.value = Math.sin(display1.value);
+	}
+	else if(document.getElementById('rad').value="rad"){
+	previous.value = "sind("+display1.value+")";
+	display1.value = Math.sin(display1.value * (Math.PI/180));
+	}else{
+		display1.value;
+	}
+}
+
+function cos(){
+	previous.value = "cos("+display1.value+")";
+	display1.value = Math.cos(display1.value);
+}
+function tan(){
+	previous.value = "tan("+display1.value+")";
+	display1.value = Math.tan(display1.value);
+}
+function sec(){
+	previous.value = "sec("+display1.value+")";
+    display1.value = 1/Math.cos(display1.value);
+}
+function cosec(){
+	previous.value = "cosec("+display1.value+")";
+    display1.value = 1/Math.sin(display1.value);
+}
+function cot(){
+	previous.value = "cot("+display1.value+")";
+    display1.value = 1/Math.tan(display1.value);
+}
+function sinh(){
+	previous.value = "sinh("+display1.value+")";
+    display1.value = Math.sinh(display1.value);
+}
+function cosh(){
+	previous.value = "cosh("+display1.value+")";
+    display1.value = Math.cosh(display1.value);
+}
+function tanh(){
+	previous.value = "tanh("+display1.value+")";
+    display1.value = Math.tanh(display1.value);
+}
+function sech(){
+	previous.value = "sech("+display1.value+")";
+    display1.value = 1/Math.sinh(display1.value);
+}
+function cosech(){
+	previous.value = "cosech("+display1.value+")";
+    display1.value = 1/Math.cosech(display1.value);
+}
+function coth(){
+	previous.value = "coth("+display1.value+")";
+    display1.value = 1/Math.tanh(display1.value);
+}
+function sinin(){
+	previous.value = "sin-1"+ "("+display1.value+")";
+    if(display1.value > -1 && display1.value < 1) {
+    display1.value = Math.asin(display1.value);
+    }
+    else{
+        display1.value = "Invalid input";
+    }
+}
+function cosin(){
+    if(display1.value > -1 && display1.value < 1) {
+        display1.value = Math.acos(display1.value);
+        }
+        else{
+            display1.value = "Invalid input";
+        }
+}
+function tanin(){
+    if(display1.value > -1 && display1.value < 1) {
+        display1.value = Math.atan(display1.value);
+        }
+        else{
+            display1.value = "Invalid input";
+        }
+}
+function secin(){
+        display1.value = Math.acos(1/display1.value);
+}
+function cosecin(){
+    
+        display1.value = Math.asin(1/display1.value);
+    
+}
+function cotin(){
+        display1.value = Math.atan(1/display1.value);
+       
+}
+function mplus(){
+	x += eval(display1.value);
+	console.log(x);
+}
+function mminus(){
+	x -= eval(display1.value);
+	console.log(x);
+}
+function ms(){
+	try{
+		x = eval(display1.value);
+		console.log(display1.value);
+	}
+	catch(error){
+		display1.value = "error";
+	}
+}
+function mc(){
+	console.clear();
+	x=0;
+}
+function mr(){
+	display1.value = x;
+}
+function fe(){
+	display1.value = parseFloat(display1.value).toExponential();
 }
