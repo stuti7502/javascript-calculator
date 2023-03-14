6165f344aee5f32c9a9835de8e499c62cf743340
Stuti Vithlani <me@stutivithlani.com>
2023-03-14 16:28:40 +0530

    final change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ buttons.forEach((item) => {
 		val = e.target.value;
 		var expression = display1.value;
 		if(e.target.value == '='){
-			
+// log function
 			const func = expression.split(" ");
 			var a = func[0];
 			var b = func[3];
@@ -25,7 +25,9 @@ buttons.forEach((item) => {
 					previous.value = display1.value;
 					display1.value = c / d;
 				}
+//root function
 			}else if(func[1]==="root"){
+					previous.value = display1.value;
 					display1.value = Math.pow(a, 1/b);
 			}
 			else{
@@ -43,6 +45,7 @@ buttons.forEach((item) => {
 		else if(e.target.value == 'rad'){
 			display1.value = "";
 		}
+//clear
 		else if(e.target.value == 'C'){
 			val = "";
 			document.getElementById('previous').value = "";
@@ -51,6 +54,7 @@ buttons.forEach((item) => {
 		else if(display1.value==="0"){
 			display1.value = val;
 		}
+//operator should not repeat
 		else{
 			let lastChar = display1.value.charAt(display1.value.length - 1);
 			let op = ["+", "-", "*", "/", "%", "."];
@@ -59,7 +63,7 @@ buttons.forEach((item) => {
 			}
 			else{
 				display1.value += val;
-				}
+			}
 		}	
 	}
 )}
@@ -195,19 +199,17 @@ function hyp(){
 	}
 };
 
-
+//find factorial 
 function fact(){
 	previous.value = "fact("+display1.value+")";
 	if (display1.value < 0) {
 		display1.value = "Error!";
 	}
 	
-	// if number is 0
 	else if (display1.value === 0) {
 		display1.value = "1";
 	}
 	
-	// if number is positive
 	else {
 		let fact = 1;
 		for (i = 1; i <= display1.value; i++) {
@@ -216,61 +218,75 @@ function fact(){
 		display1.value = fact;
 	}
 }
+//number is plus then convert it to minus and vice versa
 function plusminus(){
 	display1.value = -(display1.value)
 }
+//square of a number
 function square(){
 	previous.value = "square("+display1.value+")";
 	display1.value = display1.value ** 2;
 }
+//cube of a number
 function cube(){
 	previous.value = "cube("+display1.value+")";
 	display1.value = display1.value ** 3;
 }
+//ten to the power any number
 function tenpowerx(){
-	previous.value = "10^"+display1.value;
+	previous.value = "10 ^"+display1.value;
 	display1.value = 10**display1.value;
 }
+//two to the power any number
 function twopowerx(){
-	previous.value = "2^"+display1.value;
+	previous.value = "2 ^"+display1.value;
 	display1.value = 2**display1.value;
 }
+//remove one character at a time
 function backspace(){
 	display1.value = display1.value.substr(0, display1.value.length - 1);
 }
+//ln function
 function ln(){
 	previous.value = "ln("+display1.value+")";
 	display1.value = Math.log(display1.value);
 }
+//log function
 function log(){
 	previous.value = "log("+display1.value+")";
 	display1.value = Math.log10(display1.value);
 }
+//square root of a number
 function sqroot(){
 	previous.value = "squareroot("+display1.value+")";
 	display1.value = Math.sqrt(display1.value);
 }
+//cube root of a number
 function cbroot(){
 	previous.value = "cuberoot("+display1.value+")";
 	display1.value = Math.cbrt(display1.value);
 }
+//one divide by x 
 function onebyx(){
 	previous.value = "1/"+display1.value;
 	display1.value = 1/display1.value;
 }
+//convert any number to positive
 function abs(){
 	previous.value = "abs("+display1.value+")";
 	display1.value = Math.abs(display1.value);
 }
+//next nearest integer
 function floor(){
 	previous.value = "floor("+display1.value+")";
 	display1.value = Math.floor(display1.value);
 }
+//previous nearest integer
 function ceil(){
 	previous.value = "ceil("+display1.value+")";
 	display1.value = Math.ceil(display1.value);
 }
-
+//sin function
 function sin(){
 	if(mode =="RAD"){
 		previous.value = "sin("+display1.value+")";
@@ -281,7 +297,7 @@ function sin(){
 	display1.value = Math.sin(display1.value * (Math.PI/180));
 	}
 }
-
+//cos function
 function cos(){
 	if(mode =="RAD"){
 		previous.value = "cos("+display1.value+")";
@@ -292,6 +308,7 @@ function cos(){
 	display1.value = Math.cos(display1.value * (Math.PI/180));
 	}
 }
+//tan function
 function tan(){
 	if(mode =="RAD"){
 		previous.value = "tan("+display1.value+")";
@@ -302,6 +319,7 @@ function tan(){
 	display1.value = Math.tan(display1.value * (Math.PI/180));
 	}
 }
+//sec function
 function sec(){
 	if(mode =="RAD"){
 	previous.value = "sec("+display1.value+")";
@@ -311,6 +329,7 @@ function sec(){
 	display1.value = 1/Math.cos(display1.value * (Math.PI/180));
 	}
 }
+//cosec function
 function cosec(){
 	if(mode =="RAD"){
 		previous.value = "cosec("+display1.value+")";
@@ -320,6 +339,7 @@ function cosec(){
 		display1.value = 1/Math.sin(display1.value * (Math.PI/180));
 		}
 }
+//cot function
 function cot(){
 	if(mode =="RAD"){
 		previous.value = "cot("+display1.value+")";
@@ -329,30 +349,37 @@ function cot(){
 		display1.value = 1/Math.tan(display1.value * (Math.PI/180));
 		}
 }
+//sinh function
 function sinh(){
 	previous.value = "sinh("+display1.value+")";
     display1.value = Math.sinh(display1.value);
 }
+//cosh function
 function cosh(){
 	previous.value = "cosh("+display1.value+")";
     display1.value = Math.cosh(display1.value);
 }
+//tanh function
 function tanh(){
 	previous.value = "tanh("+display1.value+")";
     display1.value = Math.tanh(display1.value);
 }
+//sech function
 function sech(){
 	previous.value = "sech("+display1.value+")";
     display1.value = 1/Math.sinh(display1.value);
 }
+//cosech function
 function cosech(){
 	previous.value = "cosech("+display1.value+")";
     display1.value = 1/Math.cosech(display1.value);
 }
+//coth function
 function coth(){
 	previous.value = "coth("+display1.value+")";
     display1.value = 1/Math.tanh(display1.value);
 }
+//sinh function
 function sinin(){
 	previous.value = "sin-1("+display1.value+")";
     if(display1.value > -1 && display1.value < 1) {
@@ -362,6 +389,7 @@ function sinin(){
         display1.value = "Invalid input";
     }
 }
+//cos inverse function
 function cosin(){
 	previous.value = "cos-1("+display1.value+")";
     if(display1.value > -1 && display1.value < 1) {
@@ -371,6 +399,7 @@ function cosin(){
             display1.value = "Invalid input";
         }
 }
+//tan inverse function
 function tanin(){
 	previous.value = "tan-1("+display1.value+")";
     if(display1.value > -1 && display1.value < 1) {
@@ -380,20 +409,24 @@ function tanin(){
             display1.value = "Invalid input";
         }
 }
+//sec inverse function
 function secin(){
 		previous.value = "sec-1("+display1.value+")";
         display1.value = Math.acos(1/display1.value);
 }
+//cosec inverse function
 function cosecin(){
 		previous.value = "cosec-1("+display1.value+")";
         display1.value = Math.asin(1/display1.value);
     
 }
+//cot inverse function
 function cotin(){
 		previous.value = "cot-1("+display1.value+")";
         display1.value = Math.atan(1/display1.value);
        
 }
+//M+ function adding number memory
 function mplus(){
 	x += eval(display1.value);
 	console.log(x);
@@ -404,10 +437,12 @@ function mplus(){
     mr_element.classList.add("fontWt");
     mr_element.classList.remove("btnDark");
 }
+//M- function removing number from memory
 function mminus(){
 	x -= eval(display1.value);
 	console.log(x);
 }
+//store in memory
 function ms(){
 	try{
 		x = eval(display1.value);
@@ -423,6 +458,7 @@ function ms(){
     mr_element.classList.add("fontWt");
     mr_element.classList.remove("btnDark");
 }
+//clear from memory
 function mc(){
 	console.clear();
 	x=0;
@@ -433,6 +469,7 @@ function mc(){
     mr_element.classList.remove("fontWt");
     mr_element.classList.add("btnDark");
 }
+//show record of memory
 function mr(){
 	display1.value = x;
 }
@@ -440,6 +477,7 @@ function mr(){
 function fe(){
 	display1.value = parseFloat(display1.value).toExponential();
 }
+//exp function
 function exp(){
 	var expression = display1.value;
 	const eval1 = expression.split(".");
@@ -450,6 +488,7 @@ function exp(){
 	display1.value += ".e+0"
 	}
 }
+//dms function
 function ConvertDDToDMS()
 {
 	previous.value = "dms("+display1.value+")";
@@ -463,6 +502,7 @@ function deg(){
 	var pi = Math.PI;
    display1.value = display1.value * (180/pi);
 }
+//random number form 0-1
 function rand(){
 	display1.value = Math.random(display1.value);
 }
